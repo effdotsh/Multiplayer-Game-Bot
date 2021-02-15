fd3398ebc51c7339a62b8472fd5b98b87119dc94
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2021-02-15 18:45:15 -0500

    dashing when bullet about to hit:

--- bot.py
+++ bot.py
@@ -7,23 +7,31 @@ import math
 
 fire_rate = 0.4
 bullet_radius = 25
+player_radius = 50
 
 fire_timer: float = 0
 x = 0
 y = 0
-# url = 'wss://aispawn.herokuapp.com/ws'
-url = "ws://localhost:3000/ws"
+id = ''
+url = 'wss://aispawn.herokuapp.com/ws'
+# url = "ws://localhost:3000/ws"
 
 def check_incoming(bullets, x, y):
   for b in bullets:
+    # print(b['id'])
     new_x = b['x'] + b['angle'][0]
     new_y = b['y'] + b['angle'][1]
+
+    if math.dist((new_x, new_y), (x, y)) <= (bullet_radius+player_radius)*1.5 and b['fired_by']!=id:
+      ws.send(f'vel{random.randint(-100000,100000)},{random.randint(-100000,100000)}')
+      ws.send('dash')
 
 
 def ws_handler(ws, message):
   global fire_timer
   global x
   global y
+  global id
   msg=json.loads(message)
   if msg['type']=='sync_player':
       
@@ -35,11 +43,11 @@ def ws_handler(ws, message):
 
       bx=players[this_player]['x']
       by=players[this_player]['y']
-
+      id = players[this_player]['id']
       x = bx
       y = by
       if time.time() - fire_timer >= fire_rate:
-        # ws.send(f'fire{px-bx}, {py-by}')
+        ws.send(f'fire{px-bx}, {py-by}')
         fire_timer=time.time()
       ws.send(f'vel{px-bx},{py-by}')
   if msg['type']=='sync_bullet':
