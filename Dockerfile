da4e13f46e4b90da2018d8fdc73fd9b9b6cded0d
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2021-02-17 18:18:36 -0500

    docker, better collision detection

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# For more information, please refer to https://aka.ms/vscode-docker-python
+FROM python:3.8-slim-buster
+
+# Keeps Python from generating .pyc files in the container
+ENV PYTHONDONTWRITEBYTECODE=1
+
+# Turns off buffering for easier container logging
+ENV PYTHONUNBUFFERED=1
+
+# Install pip requirements
+COPY requirements.txt .
+RUN python -m pip install -r requirements.txt
+
+COPY bot.py .
+
+# # Switching to a non-root user, please refer to https://aka.ms/vscode-docker-python-user-rights
+# RUN useradd appuser && chown -R appuser .
+# USER appuser
+
+# During debugging, this entry point will be overridden. For more information, please refer to https://aka.ms/vscode-docker-python-debug
+CMD ["python", "bot.py"]
--- bot.py
+++ bot.py
@@ -4,18 +4,22 @@ import random
 import json
 import _thread as thread
 import math
+import sympy
+from sympy import *
+from sympy.geometry import *
 
-dash_forsight = 5
-dash_accelerator = 1
-dash_thresh = 3
+dash_forsight = 10
+dash_thresh = 1
 
 fire_rate = 0.4
 bullet_radius = 25
-bullet_speed =15
+bullet_speed = 15
 player_radius = 50
 
-boom = 400
-boom_thresh = 30
+radius = (player_radius + bullet_radius) * dash_thresh
+
+boom = 200
+boom_thresh = 20
 
 fire_timer: float = 0
 x = 0
@@ -23,95 +27,142 @@ y = 0
 px = 0
 py = 0
 id = ''
-url = 'wss://aispawn.herokuapp.com/ws'
+url = 'ws://aispawn.herokuapp.com/ws'
 # url = "ws://localhost:3000/ws"
+
+name = 'BotBoio'
+
+z = 0
 
 
 def random_dash(x, y, px, py):
-  connecting_slope = (y-py)/(x-px)
-  slope = -1/connecting_slope
+    connecting_slope = (y - py) / (x - px)
+    slope = -1 / connecting_slope
 
-  orbit_x = x + (px-x)/7
-  orbit_y = y + (py+y)/7
+    orbit_x = x + (px - x) / 7
+    orbit_y = y + (py + y) / 7
 
-  mid_point = ((x+px)/2)
-  diff = abs(x-px)
-  dash_x = mid_point + (diff/3 + random.randint(int(diff/6), int(diff/2)))*random.choice((-1, 1))
+    mid_point = ((x + px) / 2)
+    diff = abs(x - px)
+    dash_x = mid_point + (diff / 4 + random.randint(int(diff / 6), int(diff / 2))) * random.choice((-1, 1))
 
-  dash_y = slope*(dash_x - mid_point) + (py+y)/2
+    dash_y = slope * (dash_x - mid_point) + (py + y) / 2
 
-  ws.send(f'vel{dash_x-x},{dash_y-y}')
-  ws.send('dash')
+    ws.send(f'vel{dash_x - x},{dash_y - y}')
+    ws.send('dash')
+
 
 def check_incoming(bullets, x, y, px, py):
-  for b in bullets:
-    # print(b['id'])
-    if b['fired_by']!=id:
-      new_x = b['x'] + b['angle'][0] * dash_accelerator
-      new_y = b['y'] + b['angle'][1] * dash_accelerator
+    for b in bullets:
+        if b['fired_by'] != id and 0 < b['x'] < 3000 and 0 < b['y'] < 3000 and b != bullets[0]:
+            new_x = b['x']
+            new_y = b['y']
 
-      if (math.dist((new_x, new_y), (x, y)) <= (bullet_radius+player_radius)*dash_thresh or  math.dist((b['x'], b['y']), (x, y)) <= (bullet_radius+player_radius)*dash_thresh):
-        random_dash(x, y, px, py)
+            collide = False
+            for i in range(dash_forsight):
+                new_x += b['angle'][0]
+                new_y += b['angle'][1]
+                if math.dist((x, y), (new_x, new_y)) <= radius * dash_thresh:
+                    collide = true
+                    break
+
+            if collide:
+                # print((collision))
+                random_dash(x, y, px, py)
+
+
 def fire(x, y, px, py, vel_x, vel_y):
-  dist = math.dist((x, y), (px, py))
-  travel_time = dist/bullet_speed
-  target_x = px + vel_x*travel_time
-  target_y = py + vel_y*travel_time
-  ws.send(f'fire{target_x-x}, {target_y-y}')
+    dist = math.dist((x, y), (px, py))
+    travel_time = dist / bullet_speed
+    target_x = px + vel_x * travel_time
+    target_y = py + vel_y * travel_time
+    ws.send(f'fire{target_x - x}, {target_y - y}')
+
+
+def filter_players(player):
+    if player['spectating'] or not player['living']:
+        return False
+    else:
+        return True
 
 
 def ws_handler(ws, message):
-  global fire_timer
-  global x
-  global y
-  global px
-  global py
-  global id
-  msg=json.loads(message)
-  if msg['type']=='sync_player':
-      this_player = msg['you_are']
-      other = 1-this_player
-      players = msg['info'][0]
-      px = players[other]['x']
-      py = players[other]['y']
+    global fire_timer
+    global x
+    global y
+    global px
+    global py
+    global id
 
-      bx=players[this_player]['x']
-      by=players[this_player]['y']
-      id = players[this_player]['id']
-      x = bx
-      y = by
-      # print(players[other])
-      if(players[other]['living']):
-        if time.time() - fire_timer >= fire_rate:
-          fire(bx, by, px, py, players[other]['vel_x'], players[other]['vel_y'])
-          fire_timer=time.time()
+    msg = json.loads(message)
+    if msg['type'] == 'sync_player':
+        this_player = msg['you_are']
+        players = msg['info'][0]
 
-        if(math.dist((px, py),(bx, by)) > boom+boom_thresh):
-          ws.send(f'vel{px-bx},{py-by}')
-        elif(math.dist((px, py),(bx, by)) < boom-boom_thresh):
-          ws.send(f'vel{bx-px},{by-py}')
+        bx = players[this_player]['x']
+        by = players[this_player]['y']
+        id = players[this_player]['id']
+        x = bx
+        y = by
+
+        filtered = filter(filter_players, players)
+
+        other = 0
+        p_counter = 0
+        dist = 10000
+        other_id = players[this_player]['id']
+        for p in filtered:
+            if p['id'] != id and math.dist((p['x'], p['y']), (x, y)) < dist:
+                other_id = p['id']
+
+        for p in players:
+            if p['id'] == other_id:
+                other = p_counter
+            p_counter += 1
+
+        px = players[other]['x']
+        py = players[other]['y']
+
+        if (players[other]['living'] and other != this_player):
+            if time.time() - fire_timer >= fire_rate:
+                fire(bx, by, px, py, players[other]['vel_x'], players[other]['vel_y'])
+                fire_timer = time.time()
+
+            if (math.dist((px, py), (bx, by)) > boom + boom_thresh):
+                ws.send(f'vel{px - bx},{py - by}')
+            elif (math.dist((px, py), (bx, by)) < boom - boom_thresh):
+                ws.send(f'vel{bx - px},{by - py}')
+            else:
+                ws.send('vel0,0')
+
         else:
-          ws.send('vel0,0')
+            ws.send(f'vel{2144 / 2 - bx},{1047 / 2 - by}')
 
-      else:
-        ws.send(f'fire{random.randint(-1000, 1000)}, {random.randint(-1000, 1000)}')
-      
-  if msg['type']=='sync_bullet':
-    bullets = msg['info'][0]
-    check_incoming(bullets, x, y, px, py)
+            ws.send(f'fire{random.randint(-1000, 1000)}, {random.randint(-1000, 1000)}')
+
+    if msg['type'] == 'sync_bullet':
+        bullets = msg['info'][0]
+        check_incoming(bullets, x, y, px, py)
+
 
 def on_error(ws, error):
     print(error)
 
+
 def on_open(ws):
     def run(*args):
-      while True:
-        time.sleep(0.1)
-        ws.send('sync')
-    ws.send('nameBotBoio')
+        while True:
+            time.sleep(0.1)
+            try:
+                ws.send('sync')
+            except Exception as e:
+                print(e)
+                quit()
+
+    ws.send(f'name{name}')
     thread.start_new_thread(run, ())
 
-ws = WebSocketApp(url, on_message=ws_handler, on_error = on_error)
+
+ws = WebSocketApp(url, on_message=ws_handler, on_error=on_error)
 ws.on_open = on_open
 ws.run_forever()
-
